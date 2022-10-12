36bd6a0082ad58b1d50f0a61114a2647608c8840
Jerry Wang <x.jerry.wang@gmail.com>
2022-10-12 23:35:46 +0800

    perf: tweak tag style

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -3,8 +3,8 @@ import { RenderableProps } from 'preact'
 const tw = String.raw
 
 const colorMap: Record<string, string> = {
-  default: tw`border-gray-100 bg-gray-50 text-gray-500`,
-  blue: tw`border-blue-100 bg-blue-50 text-blue-500`
+  default: tw`border-gray-100 bg-gray-50 text-gray-500 hover:text-gray-700`,
+  blue: tw`border-blue-100 bg-blue-50 text-blue-500 hover:text-blue-700`
 }
 
 export function Tag(props: RenderableProps<{ color?: string; href?: string }>) {
