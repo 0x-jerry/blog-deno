438e98e0c4bd7e111ef89a48503346b651064bde
Jerry Wang <x.jerry.wang@gmail.com>
2022-12-03 20:55:24 +0800

    feat: implement random image api

--- routes/api/random.ts
+++ routes/api/random.ts
@@ -1,14 +1,64 @@
 import { Handlers } from '$fresh/server.ts'
 
 export const handler: Handlers = {
-  GET(_, _ctx) {
-    const seed = Math.random().toString(16).slice(2)
+  async GET(_, _ctx) {
+    const imageUrl = await getImage()
 
     return new Response(null, {
       status: 302,
       headers: {
-        location: 'https://api.0x-jerry.icu/img/random?id=' + seed
+        'cache-control': 'public, max-age=60',
+        location: imageUrl
       }
     })
   }
+}
+
+const prefix = ['https://cdn.jsdelivr.net/gh/']
+
+const cdnRoot = prefix[0] + '/0x-jerry/static/'
+
+const repoFilesUrl =
+  'https://api.github.com/repos/0x-jerry/static/git/trees/main?recursive=1'
+
+async function getAllImages() {
+  const data = await get<GithubTreeType>(repoFilesUrl)
+  const files = data.tree
+    .filter((f) => f.type === 'blob' && /^images/.test(f.path))
+    .map((f) => cdnRoot + f.path)
+
+  return files
+}
+
+async function getImage(id = Math.random().toString()) {
+  const images = await getAllImages()
+
+  let idx = new Date().getTime()
+
+  for (const c of String(id)) {
+    idx += c.charCodeAt(0)
+  }
+
+  return images[idx % images.length]
+}
+
+async function get<T = any>(url: string): Promise<T> {
+  const r = await fetch(url)
+  return r.json()
+}
+
+export interface GithubTreeType {
+  sha: string
+  url: string
+  tree: Tree[]
+  truncated: boolean
+}
+
+export interface Tree {
+  path: string
+  mode: string
+  type: string
+  sha: string
+  size?: number
+  url: string
 }
