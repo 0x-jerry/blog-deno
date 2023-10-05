5ddeb439a24c65fd4d83e18ea65b786807f7e898
Jerry <x.jerry.wang@gmail.com>
2023-10-05 05:44:07 +0000

    fix: button style

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,11 +3,11 @@ import { JSX } from 'preact'
 export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className={[
-        'px-2 py-1 border(gray-100 1) bg-gray-100 hover:bg-gray-200 inline-flex items-center justify-center',
-        props.class
-      ].join(' ')}
       {...props}
+      class={[
+        'px-2 py-1 border(gray-100 1) bg-gray-100 hover:bg-gray-200 inline-flex items-center justify-center',
+        props.class,
+      ].join(' ')}
     />
   )
 }
