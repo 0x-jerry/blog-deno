64312011edf6cf0a49697c5df19f2a614c2a3539
Jerry <x.jerry.wang@gmail.com>
2023-10-05 21:47:35 +0800

    fix: broken images

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,5 +1,6 @@
 // routes/_middleware.ts
 import { MiddlewareHandlerContext } from '$fresh/server.ts'
+import { fromFileUrl } from '$std/path/from_file_url.ts'
 import { i18nConf } from '../lib/i18n.ts'
 
 interface State {
@@ -10,10 +11,35 @@ export async function handler(
   req: Request,
   ctx: MiddlewareHandlerContext<State>
 ) {
-  const lng = new URL(req.url).searchParams.get('lng')
+  const u = new URL(req.url)
+
+  const lng = u.searchParams.get('lng')
   i18nConf.lng = lng || i18nConf.lng
 
-  const resp = await ctx.next()
+  const resp = (await handleImage(u)) || (await ctx.next())
 
   return resp
+}
+
+async function handleImage(u: URL) {
+  const imageReg = /\.(png|jpg|jpeg|webp)$/
+
+  // if is a image
+  if (u.pathname.startsWith('/posts/') && imageReg.test(u.pathname)) {
+    const relativePath = u.pathname.slice(1)
+    const ext = u.pathname.split('.').pop()
+    const buf = await readPostImage(relativePath)
+
+    return new Response(buf, {
+      headers: {
+        'content-type': `image/${ext}`
+      }
+    })
+  }
+}
+
+async function readPostImage(relativePath: string) {
+  const p = fromFileUrl(import.meta.resolve('../docs/' + relativePath))
+  const buf = await Deno.readFile(p)
+  return buf
 }
